e29bd8677911dbc84b6433b665214436efc42990
TiagoJoslin <contatojoslin@gmail.com>
2026-03-05 21:23:52 -0300

    Trator

--- Trator.java
+++ Trator.java
@@ -1,15 +1,33 @@
 package main;
 
 public class Trator extends Carro{
-    private String tipo;
-    private float tamanho;
+	private String tipo; 
+	private String tamanho;
+	
+	public Trator(String marca, String cor, int ano, String tipo, String tamanho) {
+		super(marca, cor, ano);
+		this.tipo = tipo;
+		this.tamanho = tamanho;
+	}
+	
+		public String getTipo() {
+		return tipo;
+	}
 
-    public Trator(String marca, String cor, int ano, String tipo, int tamanho){
-        super(marca, cor, ano);
-        this.tipo = tipo;
-        this.tamanho = tamanho;
-    }
-    public String buzinar(){
-        return ("bum buum")
-    }
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public String getTamanho() {
+		return tamanho;
+	}
+
+	public void setTamanho(String tamanho) {
+		this.tamanho = tamanho;
+	}
+
+		@Override
+	    public String buzinar() {
+	        return ("Bum Buumm!");
+	}
 }
